11d70e9e0f26e3d84218338f8e1b3920b3f95ad2
Daniel Shatkin <Daniel.Shatkin@gmail.com>
2013-12-20 16:29:27 +0100

    Triangle Objekt und leeren Test erstellt.

---
+++ Triangle.java
@@ -0,0 +1,21 @@
+package QSVU_UEbung3_1127230_Shatkin_Daniel;
+
+public class Triangle {
+	
+	public Triangle(){}
+	
+	public double calculateHypotenuse(double a, double b){
+		return b;
+	}
+	
+	public double calculateAngle(double angleGiven){
+		return angleGiven;
+		
+	}
+	
+	public double calculateSurface(double a, double b, double c){
+		return c;
+		
+	}
+	
+}
---
+++ TriangleTest.java
@@ -0,0 +1,19 @@
+package QSVU_UEbung3_1127230_Shatkin_Daniel;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class TriangleTest {
+	
+	Triangle triangle;
+	
+	
+	@BeforeClass
+	public void set_up(){
+		triangle = new Triangle();
+	}
+
+
+}
