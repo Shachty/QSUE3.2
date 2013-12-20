65a71737f69c13fc2d83b826778b22fe45f7edc1
Daniel Shatkin <Daniel.Shatkin@gmail.com>
2013-12-20 16:56:37 +0100

    Tests von calculatesHypotenuse fertig.

--- TriangleTest.java
+++ TriangleTest.java
@@ -1,7 +1,7 @@
 package QSVU_UEbung3_1127230_Shatkin_Daniel;
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -10,10 +10,30 @@ public class TriangleTest {
 	Triangle triangle;
 	
 	
-	@BeforeClass
+	@Before
 	public void set_up(){
 		triangle = new Triangle();
 	}
 
+	@Test
+	public void calculatesHypotenuse_shouldReturnHypotenuse(){
+		double a = 3;
+		double b = 4;
+		if(triangle.calculateHypotenuse(a, b) != 5){ fail();}
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void calculatesHypotenuseWithNegativeValues_shouldThrowException(){
+		double a = -3;
+		double b = -4;
+		
+		triangle.calculateHypotenuse(a, b);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void calculatesHypotenuseWithAZeroValue_shouldThrowException(){
+		double a = 0;
+		double b = -4;
+		
+		triangle.calculateHypotenuse(a, b);
+	}
 
 }
